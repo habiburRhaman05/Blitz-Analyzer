82af1b525a4296b9d8e214871c08cfde1d941c5d
habiburRhaman05 <devhabib2005@gmail.com>
2026-03-26 22:38:44 +0600

    clean profile page

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -1,14 +1,6 @@
-import AccountPage from '@/components/modules/user/ProfilePage'
-import httpClient from '@/lib/axios-client';
-import { getMe } from '@/services/auth.services';
-import { cookies } from 'next/headers'
-import React from 'react'
+import AccountPage from '@/components/modules/user/ProfilePage';
 
 const page = async() => {
-
-
-  const getUserProfile = await getMe();
-  console.log(getUserProfile());
   
 
   return (
